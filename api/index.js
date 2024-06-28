e15541f6939cf01413b8fddb6c9a977d4e87cb65
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-06-28 20:27:04 +0530

    mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+mongoose.connect(process.env.MONGO), then(() => {
+    console.log("connected to mongodb");
+}).catch((err) => {
+    console.log(err);
+})
 const app = express();
 app.listen(3000, () => {
     console.log("server listening on port 3000");
