3c3dce92b441a237689181824ac089134b612ed0
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-06-29 16:45:32 +0530

    mongoose error fix

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(process.env.MONGO), then(() => {
-    console.log("connected to mongodb");
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("connected to database");
+    console.log("Hello from mongoDB");
 }).catch((err) => {
     console.log(err);
-})
+});
 const app = express();
 app.listen(3000, () => {
     console.log("server listening on port 3000");
