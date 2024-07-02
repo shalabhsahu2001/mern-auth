04a3777bd0df68ad27e73d175beb11c255e92ec0
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-03 00:37:41 +0530

    added middlewares and method to handle errors

--- api/index.js
+++ api/index.js
@@ -18,5 +18,17 @@ app.listen(3000, () => {
     console.log("server listening on port 3000");
 });
 
+
+
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+});
