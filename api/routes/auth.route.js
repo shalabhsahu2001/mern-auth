be3c9094453b0f1acba7ea9487c540db117261ca
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-05 16:33:57 +0530

    Signin api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,5 +3,7 @@ import { signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 router.post("/signup", signup);
+router.post("/signin", );
+
 
 export default router;
