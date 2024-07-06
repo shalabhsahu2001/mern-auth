b853301cf09d1bb0b1c3adb5e624c61eca22839c
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-06 23:50:48 +0530

    added cookie ...completed signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,10 @@ export const signin = async (req, res, next) => {
         if(!validUser) return next(errorHandler(404, 'user not found'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword) return next(errorHandler(401, 'Wrong Credentials'));
-        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET,)
+        const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest} = validUser._doc;
+        const expiryDate = new Date(Date.now() + 3600000);
+        res.cookie('access_token', token, {httpOnly: true, expires: expiryDate}).status(200).json(rest);
     } catch (error) {
         next(error);
     }
