755d5513c6c09b94a44a59c49ca642b53431db44
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-10 00:03:26 +0530

    redux

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    loading: false,
+    error: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers:{
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = false;
+        }
+    }
+})
