5801a43dfaf9524679fffbdec631a5d189231016
Shalabh Sahu <sahushalabh2001@gmail.com>
2024-07-25 00:12:50 +0530

    error fix signin func.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
-import { userReducer } from './user/userSlice.js';
+import userReducer from './user/userSlice.js';
 
 export const store = configureStore({
     reducer: {user: userReducer},
